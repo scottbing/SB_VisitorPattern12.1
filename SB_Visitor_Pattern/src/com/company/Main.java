2c6acc87e515d8265cc9c10db89b43718d278097
scottbing <bingscott@gmail.com>
2020-04-19 21:54:17 -0600

    Add files via upload

--- SB_Visitor_Pattern/src/com/company/Main.java
+++ SB_Visitor_Pattern/src/com/company/Main.java
@@ -1,21 +1,43 @@
 package com.company;
 
+import java.util.Scanner;
+import java.io.*;
+
+
 public class Main {
 
     public static void main(String[] args) {
 	// write your code here
 
-        TaxVisitor taxVisitor = new TaxVisitor();
+        Scanner scan = new Scanner(System.in);
 
-        Liquor blue = new Liquor(3);
-        Necessity milk = new Necessity(5);
-        Tobacco smoke = new Tobacco(7);
+        System.out.println("Tax Break? (Y/y): ");
+        String answer = scan.next();
+        if (answer.equalsIgnoreCase("y")) {
+            TaxBreakVisitor taxBreakVisitor = new TaxBreakVisitor();
+            Liquor blue = new Liquor(3);
+            Necessity milk = new Necessity(5);
+            Tobacco smoke = new Tobacco(7);
 
-        System.out.println(blue.accept(taxVisitor));
-        System.out.println(milk.accept(taxVisitor));
-        System.out.println(smoke.accept(taxVisitor));
+            System.out.println(blue.accept(taxBreakVisitor));
+            System.out.println(milk.accept(taxBreakVisitor));
+            System.out.println(smoke.accept(taxBreakVisitor));
 
-        double cart = blue.accept(taxVisitor) + milk.accept(taxVisitor) + smoke.accept(taxVisitor);
-        System.out.println("Total: " + cart);
+            double cart = blue.accept(taxBreakVisitor) + milk.accept(taxBreakVisitor) + smoke.accept(taxBreakVisitor);
+            System.out.println("Total: " + cart);
+        }else{
+            TaxVisitor taxVisitor = new TaxVisitor();
+            Liquor blue = new Liquor(3);
+            Necessity milk = new Necessity(5);
+            Tobacco smoke = new Tobacco(7);
+
+            System.out.println(blue.accept(taxVisitor));
+            System.out.println(milk.accept(taxVisitor));
+            System.out.println(smoke.accept(taxVisitor));
+
+            double cart = blue.accept(taxVisitor) + milk.accept(taxVisitor) + smoke.accept(taxVisitor);
+            System.out.println("Total: " + cart);
+        }
     }
+
 }
---
+++ SB_Visitor_Pattern/src/com/company/TaxBreakVisitor.java
@@ -0,0 +1,27 @@
+package com.company;
+
+public class TaxBreakVisitor implements Visitor {
+    @Override
+    public double visit(Liquor liquorItem) {
+        double taxAmount = 0.0367;
+        System.out.println("Liquor item price with tax ");
+        double basePrice = liquorItem.getPrice();
+        return basePrice + (taxAmount*basePrice);
+    }
+
+    @Override
+    public double visit(Necessity necessaryItem) {
+        double taxAmount = 0.001;
+        System.out.println("Necessity item price with tax ");
+        double basePrice = necessaryItem.getPrice();
+        return basePrice + (taxAmount*basePrice);
+    }
+
+    @Override
+    public double visit(Tobacco tobaccoItem) {
+        double taxAmount = 0.0034;
+        System.out.println("Tobacco item price with tax ");
+        double basePrice = tobaccoItem.getPrice();
+        return basePrice + (taxAmount*basePrice);
+    }
+}
