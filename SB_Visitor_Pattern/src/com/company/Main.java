eb6e705ebeb98b40c2c5c7416453be372b9cd95a
scottbing <bingscott@gmail.com>
2020-04-20 00:09:30 -0600

    Add files via upload

--- SB_Visitor_Pattern/src/com/company/Main.java
+++ SB_Visitor_Pattern/src/com/company/Main.java
@@ -7,37 +7,44 @@ import java.io.*;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        // write your code here
 
+        String answer;
         Scanner scan = new Scanner(System.in);
 
-        System.out.println("Tax Break? (Y/y): ");
-        String answer = scan.next();
-        if (answer.equalsIgnoreCase("y")) {
-            TaxBreakVisitor taxBreakVisitor = new TaxBreakVisitor();
-            Liquor blue = new Liquor(3);
-            Necessity milk = new Necessity(5);
-            Tobacco smoke = new Tobacco(7);
+        while (true) {
+            System.out.println("\nTax Break? (Y/y|N/n) or Quit 'Q/q");
+            answer = scan.next();
+            if (answer.equalsIgnoreCase("q")) {
+                break;
+            } else {
+                if (answer.equalsIgnoreCase("y")) {
+                    TaxBreakVisitor taxBreakVisitor = new TaxBreakVisitor();
+                    Liquor blue = new Liquor(3);
+                    Necessity milk = new Necessity(5);
+                    Tobacco smoke = new Tobacco(7);
 
-            System.out.println(blue.accept(taxBreakVisitor));
-            System.out.println(milk.accept(taxBreakVisitor));
-            System.out.println(smoke.accept(taxBreakVisitor));
+                    System.out.println(blue.accept(taxBreakVisitor));
+                    System.out.println(milk.accept(taxBreakVisitor));
+                    System.out.println(smoke.accept(taxBreakVisitor));
 
-            double cart = blue.accept(taxBreakVisitor) + milk.accept(taxBreakVisitor) + smoke.accept(taxBreakVisitor);
-            System.out.println("Total: " + cart);
-        }else{
-            TaxVisitor taxVisitor = new TaxVisitor();
-            Liquor blue = new Liquor(3);
-            Necessity milk = new Necessity(5);
-            Tobacco smoke = new Tobacco(7);
+                    double cart = blue.accept(taxBreakVisitor) + milk.accept(taxBreakVisitor) + smoke.accept(taxBreakVisitor);
+                    System.out.println("Total: " + cart);
+                } else {
+                    TaxVisitor taxVisitor = new TaxVisitor();
+                    Liquor blue = new Liquor(3);
+                    Necessity milk = new Necessity(5);
+                    Tobacco smoke = new Tobacco(7);
 
-            System.out.println(blue.accept(taxVisitor));
-            System.out.println(milk.accept(taxVisitor));
-            System.out.println(smoke.accept(taxVisitor));
+                    System.out.println(blue.accept(taxVisitor));
+                    System.out.println(milk.accept(taxVisitor));
+                    System.out.println(smoke.accept(taxVisitor));
 
-            double cart = blue.accept(taxVisitor) + milk.accept(taxVisitor) + smoke.accept(taxVisitor);
-            System.out.println("Total: " + cart);
+                    double cart = blue.accept(taxVisitor) + milk.accept(taxVisitor) + smoke.accept(taxVisitor);
+                    System.out.println("Total: " + cart);
+                }
+            }
         }
+        System.out.println("Goodbye... Thank you for using the app.");
     }
-
 }
